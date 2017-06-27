d27d88d1773c514563e122a50963629ea5ea944a
Bojan Jelaca <bojan.jelaca@zuehlke.com>
2017-06-27 13:37:42 +0200

    Initial commit

---
+++ SharedWhiteBoard/Controllers/ImageController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using SharedWhiteBoard.Resources;
+
+namespace SharedWhiteBoard.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [HttpPost]
+        [Route("ImageApi/Image")]
+        public async Task UploadImage()
+        {
+            try
+            {
+                System.IO.File.AppendAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\log.txt", "Starting upload.\n");
+
+                var image = await Request.Content.ReadAsByteArrayAsync();
+
+                var inputDirectoryFullPath =
+                    $"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.InputFolder}\\image.jpg";
+                System.IO.File.WriteAllBytes(inputDirectoryFullPath, image);
+
+                System.IO.File.AppendAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\log.txt", "Saved to input dir.\n");
+
+                var whiteBoardDetectionAppPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\WhiteBoardDetection.exe";
+                var storageFolderPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}";
+                var process = Process.Start(whiteBoardDetectionAppPath, storageFolderPath);
+
+                System.IO.File.AppendAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\log.txt", $"Started process {whiteBoardDetectionAppPath} with parameter {storageFolderPath}\n");
+
+                process.WaitForExit();
+
+                System.IO.File.AppendAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\log.txt", $"Process ended.\n");
+            }
+            catch (Exception e)
+            {
+                System.IO.File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\errors.txt", e.Message);
+                System.IO.File.AppendAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.StorageFolder}\\errors.txt", e.StackTrace);
+            }
+        }
+
+        [HttpGet]
+        [Route("ImageApi/Image")]
+        public HttpResponseMessage GetLastImage()
+        {
+            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\{Resources.Resources.OutputFolder}\\image.jpg";
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(bytes)
+            };
+
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return result;
+        }
+    }
+}
---
+++ SharedWhiteBoard/Controllers/SessionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Http;
+
+namespace SharedWhiteBoard.Controllers
+{
+    public class SessionController : ApiController
+    {
+        public static long? SessionPin;
+
+        [HttpGet]
+        [Route("SessionApi/Session")]
+        public IHttpActionResult StartSession()
+        {
+            if (SessionPin.HasValue)
+            {
+                return BadRequest("Session already started");
+            }
+
+            // TODO Put into service
+            SessionPin = 222222;
+
+            return Ok(SessionPin);
+        }
+
+        [HttpGet]
+        [Route("SessionApi/Session/{sessionPin:long}")]
+        public IHttpActionResult ConnectToExistingSession(long sessionPin)
+        {
+            if (SessionPin != sessionPin)
+            {
+                return BadRequest();
+            }
+
+            return Ok("Connection successfull");
+        }
+
+        [HttpGet]
+        [Route("SessionApi/Session/End")]
+        public IHttpActionResult EndSession()
+        {
+            SessionPin = null;
+            
+            return Ok();
+        }
+    }
+}
